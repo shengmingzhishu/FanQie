61c76419f96fccb1f02e29e6316c73e92f6d9491
liguiqiang@aliyun.com <Al1g2q3king>
2020-04-09 19:07:21 +0800

    init

---
+++ FanQer/FanQie.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FanQer
+{
+    partial class FanQie
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FanQie));
+            this.timerUtil = new System.Windows.Forms.Timer(this.components);
+            this.radioGroupBox = new System.Windows.Forms.GroupBox();
+            this.numCount = new System.Windows.Forms.NumericUpDown();
+            this.radioClose = new System.Windows.Forms.RadioButton();
+            this.radioOpen = new System.Windows.Forms.RadioButton();
+            this.timerDing = new System.Windows.Forms.Timer(this.components);
+            this.timerClose = new System.Windows.Forms.Timer(this.components);
+            this.radioGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerUtil
+            // 
+            this.timerUtil.Tick += new System.EventHandler(this.timerUtil_Tick);
+            // 
+            // radioGroupBox
+            // 
+            this.radioGroupBox.BackColor = System.Drawing.Color.Transparent;
+            this.radioGroupBox.Controls.Add(this.numCount);
+            this.radioGroupBox.Controls.Add(this.radioClose);
+            this.radioGroupBox.Controls.Add(this.radioOpen);
+            this.radioGroupBox.Location = new System.Drawing.Point(7, 6);
+            this.radioGroupBox.Name = "radioGroupBox";
+            this.radioGroupBox.Size = new System.Drawing.Size(186, 50);
+            this.radioGroupBox.TabIndex = 1;
+            this.radioGroupBox.TabStop = false;
+            this.radioGroupBox.Text = "开/关";
+            // 
+            // numCount
+            // 
+            this.numCount.Increment = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.numCount.Location = new System.Drawing.Point(125, 20);
+            this.numCount.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numCount.Name = "numCount";
+            this.numCount.Size = new System.Drawing.Size(41, 21);
+            this.numCount.TabIndex = 1;
+            this.numCount.Value = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            // 
+            // radioClose
+            // 
+            this.radioClose.AutoSize = true;
+            this.radioClose.Location = new System.Drawing.Point(77, 22);
+            this.radioClose.Name = "radioClose";
+            this.radioClose.Size = new System.Drawing.Size(35, 16);
+            this.radioClose.TabIndex = 1;
+            this.radioClose.TabStop = true;
+            this.radioClose.Text = "关";
+            this.radioClose.UseVisualStyleBackColor = true;
+            this.radioClose.CheckedChanged += new System.EventHandler(this.radioClose_CheckedChanged);
+            // 
+            // radioOpen
+            // 
+            this.radioOpen.AutoSize = true;
+            this.radioOpen.Location = new System.Drawing.Point(23, 22);
+            this.radioOpen.Name = "radioOpen";
+            this.radioOpen.Size = new System.Drawing.Size(41, 16);
+            this.radioOpen.TabIndex = 0;
+            this.radioOpen.TabStop = true;
+            this.radioOpen.Text = " 开";
+            this.radioOpen.UseVisualStyleBackColor = true;
+            this.radioOpen.CheckedChanged += new System.EventHandler(this.radioOpen_CheckedChanged);
+            // 
+            // timerDing
+            // 
+            this.timerDing.Interval = 180000;
+            this.timerDing.Tick += new System.EventHandler(this.timerDing_Tick);
+            // 
+            // timerClose
+            // 
+            this.timerClose.Interval = 90000;
+            this.timerClose.Tick += new System.EventHandler(this.timerClose_Tick);
+            // 
+            // FanQie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(199, 63);
+            this.Controls.Add(this.radioGroupBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FanQie";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "番茄定时";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FanQie_Load);
+            this.radioGroupBox.ResumeLayout(false);
+            this.radioGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerUtil;
+        private System.Windows.Forms.GroupBox radioGroupBox;
+        private System.Windows.Forms.NumericUpDown numCount;
+        private System.Windows.Forms.RadioButton radioClose;
+        private System.Windows.Forms.RadioButton radioOpen;
+        private System.Windows.Forms.Timer timerDing;
+        private System.Windows.Forms.Timer timerClose;
+    }
+}
+
---
+++ FanQer/FanQie.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FanQer
+{
+    public partial class FanQie : Form
+    {
+        public FanQie()
+        {
+            InitializeComponent();
+        }
+
+        System.Media.SoundPlayer sp = new SoundPlayer();
+        System.Media.SoundPlayer spDing = new SoundPlayer();
+
+        int timeLong = 0;
+        int dingCount = 0;
+
+        String filePath = @"C:/fqsong.wav";
+        String fileDingPath = @"C:/fqding.wav";
+        String msgNorHasFile = @"请存入结束音乐：C:/fqsong.wav";
+        String msgNorHasDingFile = @"请存入警告音乐：C:/fqding.wav";
+
+        private void radioOpen_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioOpen.Checked)
+            {
+                //判断文件是否存在  
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show(msgNorHasFile);
+                    this.radioClose.Checked = true;
+                    return;
+                }
+                //判断文件是否存在  
+                if (!File.Exists(fileDingPath))
+                {
+                    MessageBox.Show(msgNorHasDingFile);
+                    this.radioClose.Checked = true;
+                    return;
+                }
+                this.timerUtil.Enabled = true;
+
+                timeLong = int.Parse(this.numCount.Value.ToString());
+
+                dingCount = 1;
+                // 设置倒计时 3/18
+                this.radioGroupBox.Text = dingCount+"/" + timeLong;
+
+                // 设置结束音乐
+                // this.timerUtil.Interval = 1000;
+                this.timerUtil.Interval = timeLong * 60 * 1000;
+                this.timerUtil.Start();
+
+                // 1分钟叮叮一次
+                this.timerDing.Interval = 1 * 60 * 1000;
+                this.timerDing.Start();
+            }
+        }
+
+        private void radioClose_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.radioClose.Checked)
+            {
+                this.radioGroupBox.Text = "开/关";
+                this.timerUtil.Enabled = false;
+                this.timerUtil.Stop();
+                this.timerDing.Stop();
+                sp.Stop();
+                spDing.Stop();
+            }
+        }
+
+        private void timerUtil_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                sp.SoundLocation = filePath;
+                sp.PlayLooping();
+
+                // 结束
+                this.radioGroupBox.Text = "开/关";
+
+                // 停止叮叮计时
+                this.timerDing.Stop();
+                // 定时关闭结束音乐
+                this.timerClose.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(msgNorHasFile);
+            }
+        }
+
+        private void timerDing_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                spDing.SoundLocation = fileDingPath;
+                spDing.Play();
+
+                // 设置倒计时 3/18
+                this.radioGroupBox.Text = (++dingCount) +"/" + timeLong;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(msgNorHasDingFile);
+            }
+        }
+
+        private void FanQie_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerClose_Tick(object sender, EventArgs e)
+        {
+            sp.Stop();
+            this.timerClose.Enabled = false;
+            this.radioClose.Checked = true;
+        }
+    }
+}
